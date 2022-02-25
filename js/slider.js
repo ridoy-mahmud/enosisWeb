eef49128484e3f8346c6401ea1d93a39b432ec4d
ridoy206 <ridoymahmud678@gmail.com>
2022-02-25 16:07:27 +0600

    website commited

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: false,
+    dots: false
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [2000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
